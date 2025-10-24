0199bdd96c3118460f26bfb17310eafc008f5949
Студент группы IS <is@sielom.local>
2025-10-24 12:05:05 +0500

    Шаблоны

--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Owner, Pet, Veterinarian, Appointment, Medication
 
-# Create your views here.
+def index(request):
+    owners = Owner.objects.all()
+    pets = Pet.objects.all()
+    veterinarians = Veterinarian.objects.all()
+    appointments = Appointment.objects.all()
+    medications = Medication.objects.all()
+
+    context = {
+        'owners': owners,
+        'pets': pets,
+        'veterinarians': veterinarians,
+        'appointments': appointments,
+        'medications': medications,
+    }
+
+    return render(request, 'index.html', context)
